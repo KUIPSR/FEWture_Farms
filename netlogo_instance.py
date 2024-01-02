ae9722825d811e766c9bd77a7865bfbfb48e4802
siva rama krishna yellepeddi <yellkrishna@gmail.com>
2024-01-02 15:34:37 -0600

    modified path to jvm

--- netlogo_instance.py
+++ netlogo_instance.py
@@ -5,7 +5,7 @@ netlogo = None
 def get_netlogo_instance():
     global netlogo
     if netlogo is None:
-        netlogo = pynetlogo.NetLogoLink(gui=False, jvm_path=r"C:\Program Files\Java\jdk-20.0.2\bin\server\jvm.dll")
+        netlogo = pynetlogo.NetLogoLink(gui=False, jvm_path=r"C:\Java\jdk-12.0.2\bin\server\jvm.dll")
         netlogo.load_model(r"netlogo\FEWCalc_Export_Test.nlogo")
         # netlogo.command("setup")
     return netlogo
